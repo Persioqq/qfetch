bda3c88ff3c903ebe440d467f1310b69386c94cf
Persioqq <henriquesstobbe@gmail.com>
2022-02-07 12:47:24 -0300

    New draw

--- qfetch.c
+++ qfetch.c
@@ -19,10 +19,10 @@ info()
 	{
 		printf("\033[33m ___ \033[m\t\033[36m___________________\033[m\n");
     		printf("\033[33m|   |\033[m\tsystem   \033[33m=>\033[m %s\n", info.sysname);
-    		printf("\033[33m|   |\033[m\thostname \033[33m=>\033[m %s\n", info.nodename);
-    		printf("\033[33m|   |\033[m\tkernel   \033[33m=>\033[m %s\n", info.release);
-		printf("\033[33m|   |\033[m\tarch     \033[33m=>\033[m %s\n", info.machine);
-		printf("\033[33m|___|\033[m\t\033[36m___________________\033[m\n");
+    		printf("\033[33m| | |\033[m\thostname \033[33m=>\033[m %s\n", info.nodename);
+    		printf("\033[33m| \\/ /\033[m\tkernel   \033[33m=>\033[m %s\n", info.release);
+		printf("\033[33m|   /\033[m\tarch     \033[33m=>\033[m %s\n", info.machine);
+		printf("\033[33m \\ /\033[m\t\033[36m___________________\033[m\n");
 
   	}
 
